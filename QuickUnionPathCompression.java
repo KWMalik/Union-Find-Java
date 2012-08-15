87c9743749d06b62a0e827625d45f30ab3c925aa
Aneesh Dogra <lionaneesh@gmail.com>
2012-08-15 15:36:31 +0530

    Fix infinite loop.

--- QuickUnionPathCompression.java
+++ QuickUnionPathCompression.java
@@ -10,11 +10,10 @@ public class QuickUnionPathCompression {
             sz[i] = 1;
         }
     }
-    
     public int root(int p) {
         int q = p;
-        while (id[p] != q) { q = id[p];} 
-        id[p] = q; // Path Compression, Planerize
+        while (id[q] != q) { q = id[q];}
+        id[p] = q; // Path Compression
         sz[q] = 2; // Change the size after path compression
         return q;
     }
@@ -33,10 +32,13 @@ public class QuickUnionPathCompression {
     public static void main(String[] args) {
         int N = 10;
         QuickUnionPathCompression q = new QuickUnionPathCompression(N);
-        q.union(1, 2);
+        q.union(3, 8);
+        q.union(5, 2);
         q.union(2, 3);
+        q.union(9, 1);
+        q.union(7, 4);
+        q.union(3, 9);
         for (int i = 0; i < N; i++) 
             System.out.print(q.id[i] + " ");
-        System.out.println(q.connected(1, 3));
     }
 }
